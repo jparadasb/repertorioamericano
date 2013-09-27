549086a491dd75df5ec17437d542724d6954cee2
pstopb <jparadas.b@gmail.com>
2013-09-27 13:19:31 -0430

    Se quitaron los logos de By y REdi/ se ajusto el banner institucional

--- app/views/layouts/base.blade.php
+++ app/views/layouts/base.blade.php
@@ -15,9 +15,10 @@
 <div class="oscuroI" id="oscuro"></div>
 	<header class="container">
 		<div class="row">
-			<div class="span3"></div>
-			<div class="span16">{{HTML::image('resources/bannergob.png')}}</div>
-			<div class="span5"></div>
+			<div class="span24">
+				<div class="gobleft">{{HTML::image('resources/bannergob_left.png')}}</div>
+				<div class="gobrigth">{{HTML::image('resources/bannergob_rigth.png')}}</div>
+		</div>
 		</div>
 		<div class="row">
 			<div class="span24" id="red"></div>
@@ -26,8 +27,8 @@
 			<div class="span10">{{HTML::image('resources/elnuevorepertorio.png')}}</div>
 			<div class="span8"></div>
 			<div class="span6">
-				<a href="http://humanidadenred.org.ve">{{HTML::image('resources/rdiadhpng.png')}}</a>
-				<a href="http://fba.org.ve">{{HTML::image('resources/ayacucho.png')}}</a>
+				<!-- <a href="http://humanidadenred.org.ve">{{HTML::image('resources/rdiadhpng.png')}}</a>
+				<a href="http://fba.org.ve">{{HTML::image('resources/ayacucho.png')}}</a> -->
 			</div>
 		</div>
 		<div class="row">
@@ -55,7 +56,6 @@
 						</li>
 						<li id=@yield('ide')>
 							<a href=@yield('enlaceslink')>ENLACES</a>
-							{{HTML::image('resources/backnav.png', '', array('id'=>'navb'))}}
 						</li>
 					</UL>
 
@@ -74,9 +74,9 @@
 					Centro Financiero Latino, piso 12. Ofics 1, 2 y 3. Avenida Urdaneta. Ánimas a Plaza España. Caracas 1010. Venezuela - Apartados Postales 14.413 y 2.122
 					 Teléfonos +58 212 561 6691 - Fax +58 212 564 5643
 				</div>
-				<div class="span1"><a href="#">{{HTML::image('resources/mail.png')}}</a></div>
-				<div class="span1"><a href="#">{{HTML::image('resources/facebook.png')}}</a></div>
-				<div class="span1"><a href="#">{{HTML::image('resources/twitter.png')}}</a></div>
+				<div class="span1"><a href="mailto:contacto@nuevorepertorioamericano.org">{{HTML::image('resources/mail.png')}}</a></div>
+				<div class="span1"><a href="https://www.facebook.com/pages/Fundaci%C3%B3n-Biblioteca-Ayacucho/140519035982104">{{HTML::image('resources/facebook.png')}}</a></div>
+				<div class="span1"><a href="https://twitter.com/biblioayacucho">{{HTML::image('resources/twitter.png')}}</a></div>
 			</div>
 		</footer>
 	</div>
