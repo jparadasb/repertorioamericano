9e3b764c37aeb43587d542d8a742173ecb0f66e4
pstopb <jparadas.b@gmail.com>
2013-10-07 11:12:13 -0430

    old project

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'repertorioamericano',
+			'database'  => 'americano',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
